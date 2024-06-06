ef2dfc3dc9b24475f2f7a6a98a072210fca8e897
Janriz Libres <libres.janriz@gmail.com>
2024-06-06 08:03:54 +0800

    configured settings for forget password

--- hexadash_django/settings.py
+++ hexadash_django/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', '127.0.0.1:8000', '198.54.120.56', 'localhost']
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = '20210021661@my.xu.edu.ph'
+EMAIL_HOST_PASSWORD = 'pjcnnpyjgxifdpkf'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # Application definition
 
